26c7f625b2202e4eb9a15bcd02e2845672ad06b6
kishan kumar <rajkishan7870@gmail.com>
2023-03-30 01:51:53 +0530

    solution of react day 11

--- App.js
+++ App.js
@@ -1,29 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./components/About/About";
-import Register from "./components/Register/Register";
-import Navbar from "./components/Navbar/Navbar";
-import Login from "./components/Login/Login";
-
+import Quiz from "./components/Quiz";
+import './App.css'
 
 
 function App() {
 
-  
-
   return(
-    <BrowserRouter>
-      <Navbar/>
-      <Routes>
 
-          <Route path="/About" element={<About />}/>
+    <div>
 
-          <Route path="/Register" element={<Register />}/>
+      <Quiz/>
 
-          <Route path="/Login" element={<Login />}/>
-        </Routes>
-
-    </BrowserRouter>
+    </div>
+    
   )
   
 }
