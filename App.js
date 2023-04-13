2bd7470eee1be12159a9a429759d11170af99823
kishan kumar <rajkishan7870@gmail.com>
2023-04-13 22:19:10 +0530

    solution of day-4 rev

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-import Form from "./components/form";
+import Context from "./components/context";
 
 
 
@@ -11,7 +10,7 @@ function App() {
 
     <div className='App'>
       
-        <Form/>
+        <Context/>
 
     </div>
 
